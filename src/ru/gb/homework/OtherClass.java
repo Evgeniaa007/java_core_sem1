ae2ec497708b004cd2ca82b740c752a68406d91e
Evgeniia <jonnydor@gmail.com>
2024-11-14 23:27:46 +0300

    внесены исправления в докуменацию

--- src/ru/gb/homework/OtherClass.java
+++ src/ru/gb/homework/OtherClass.java
@@ -7,15 +7,31 @@ import java.util.List;
 public class OtherClass {
 
     public static List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+
+    /**
+     * метод добавления элемента
+     * @param num - число, которое добаляем
+     * @return
+     */
     public static List<Integer> addElement(int num){
         list.add(num);
         return list;
     }
 
+    /**
+     * метод получения значения
+     * элемента по индексу
+     * @param index
+     * @return
+     */
     public static int getElement(int index){
        return list.get(index);
     }
 
+    /**
+     * метод получения размера списка
+     * @return
+     */
     public static int listSize(){
         return list.size();
     }
--- src/ru/gb/homework/View.java
+++ src/ru/gb/homework/View.java
@@ -4,7 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class View {
-
+    /**
+     * печать массива
+     * и элемента, который был в него добавлен
+     * @param list
+     * @param num
+     */
     public static void printList(List<Integer> list, int num){
         for(int el : list){
             System.out.println(el);
@@ -12,10 +17,19 @@ public class View {
         System.out.println(num + " успешно добален в массив");
     }
 
+    /**
+     * вывод значения элемента списка (по индексу)
+     * @param index
+     * @param num
+     */
     public static void printElement(int index, int num){
         System.out.println("По индексу " + index + " находится число " + num);
     }
 
+    /**
+     * вывод значения размера списка
+     * @param size
+     */
     public static void printSize(int size){
         System.out.println("Размер массива: " + size);
     }
